a3c502cd46c949f9eb393b42de06e08ed1f38038
Jackseni <50254824+Jackseni@users.noreply.github.com>
2020-12-16 03:19:03 -0600

    Add files via upload

---
+++ IntegranteModelo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Google.Cloud.Firestore;
+
+namespace FirebaseConMVC.Models
+{
+    [FirestoreData]
+    public class IntegranteModelo
+    {
+        public string Id { get; set; }
+
+        [FirestoreProperty]
+        public string Nombre { get; set; }
+    }
+}
---
+++ PacienteModelo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Google.Cloud.Firestore;
+
+namespace FirebaseConMVC.Models
+{
+    [FirestoreData]
+    public class PacienteModelo
+    {
+        public string Id { get; set; }
+
+        [FirestoreProperty]
+        public string Nombre { get; set; }
+
+        [FirestoreProperty]
+        public string Identidad { get; set; }
+
+        [FirestoreProperty]
+        public string Sexo { get; set; }
+
+        [FirestoreProperty]
+        public string Telefono { get; set; }
+
+        [FirestoreProperty]
+        public string Sintomas { get; set; }
+
+        [FirestoreProperty]
+        public string Direccion { get; set; }
+
+        [FirestoreProperty]
+        public string Fecha { get; set; }
+
+        [FirestoreProperty]
+        public string CuentaAlumno { get; set; }
+    }
+}
